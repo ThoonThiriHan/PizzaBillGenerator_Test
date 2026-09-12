e46f38adf0b21c31852bf450a14e18b22c806be6
thiri <40870395@live.napier.ac.uk>
2026-09-12 23:47:00 +0630

    First Commit

---
+++ src/Pizza.java
@@ -0,0 +1,16 @@
+public class Pizza {
+    private int price;
+    private Boolean veg;
+
+    public Pizza(Boolean veg) {
+        this.veg = veg;
+        if(this.veg){
+            this.price = 300;
+        } else{
+            this.price = 400;
+        }
+    }
+
+
+
+}
