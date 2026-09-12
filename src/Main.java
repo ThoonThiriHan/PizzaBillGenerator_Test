3d70f8f6a52c0c0ad78318a0c654ea27b8196ba6
thiri <40870395@live.napier.ac.uk>
2026-09-13 00:22:06 +0630

    Using constructor and output

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main (String[] args){
-        Pizza basedPizza = new Pizza(false);
-        basedPizza.addExtraToppings();
-        basedPizza.addExtraCheese();
-        basedPizza.takeAway();
-        basedPizza.getBill();
+//        Pizza basedPizza = new Pizza(false);
+//        basedPizza.addExtraToppings();
+//        basedPizza.addExtraCheese();
+//        basedPizza.takeAway();
+//        basedPizza.getBill();
+
+        DeluxPizza dp =  new DeluxPizza(true);
+        dp.getBill();
     }
 }
