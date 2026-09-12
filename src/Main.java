e2aba964b20afa33f4744c4533c6ccf576276fd7
thiri <40870395@live.napier.ac.uk>
2026-09-13 00:02:47 +0630

    New functions in Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main (String[] args){
         Pizza basedPizza = new Pizza(false);
+        basedPizza.addExtraToppings();
+        basedPizza.addExtraCheese();
+        basedPizza.takeAway();
+        basedPizza.getBill();
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -2,6 +2,10 @@ public class Pizza {
     private int price;
     private Boolean veg;
 
+    private int extraCheesePrice = 100;
+    private int extraToppingPrice = 150;
+    private int backPack = 20;
+
     public Pizza(Boolean veg) {
         this.veg = veg;
         if(this.veg){
@@ -11,20 +15,23 @@ public class Pizza {
         }
     }
 
-    private void addExtraChesse(){
-
+    public void addExtraCheese(){
+        System.out.println("Extra cheese added");
+        this.price += extraCheesePrice;
     }
 
     public void addExtraToppings(){
-
+        System.out.println("Extra Toppings Added");
+        this.price += extraToppingPrice;
     }
 
     public void takeAway(){
-
+        System.out.println("Take away opted");
+        this.price += backPack;
     }
 
     public void getBill(){
-
+        System.out.println(this.price);
     }
 
 
